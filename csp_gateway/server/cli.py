b0b2ab96bf34708a90910f1953f550bb4bd9d8b0
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-04-15 16:23:55 -0400

    Move to ccflow standardized load_config
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- csp_gateway/server/cli.py
+++ csp_gateway/server/cli.py
@@ -1,11 +1,28 @@
-import hydra
+import logging
+from pprint import pprint
 
-from csp_gateway.server.config import run
+import hydra
+from ccflow import ModelRegistry
+
+from csp_gateway import __version__
+
+log = logging.getLogger(__name__)
 
 
 @hydra.main(config_path="config", config_name="base", version_base=None)
 def main(cfg):
-    run(cfg)
+    log.info("Loading csp-gateway config...")
+    registry = ModelRegistry.root()
+    registry.load_config(cfg=cfg, overwrite=True)
+    gateway = registry["gateway"]
+
+    log.info(f"Starting csp_gateway version {__version__}")
+    kwargs = cfg["start"]
+    if kwargs:  # i.e. start=False override on command line
+        log.info(f"Starting gateway with arguments: {kwargs}")
+        gateway.start(**kwargs)
+    else:
+        pprint(gateway.model_dump(by_alias=True))
 
 
 if __name__ == "__main__":
